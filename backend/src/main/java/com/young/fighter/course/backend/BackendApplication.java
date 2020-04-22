7abbd5bc030e13c66161c778639e3207b8cbadf3
mbelykh <mbelykh@phoenixit.ru>
2020-04-22 17:47:17 +0300

    deleted unused code

--- backend/src/main/java/com/young/fighter/course/backend/BackendApplication.java
+++ backend/src/main/java/com/young/fighter/course/backend/BackendApplication.java
@@ -3,9 +3,7 @@ package com.young.fighter.course.backend;
 public class BackendApplication {
 
     public static void main(String[] args) {
-//        ApplicationContext ctx = new ClassPathXmlApplicationContext("hatefulXml.xml");
-//        System.out.println(ctx.getBeanDefinitionCount());
-//        System.out.println(ctx.getStartupDate());
+
     }
 
 }
